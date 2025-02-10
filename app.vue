e07a7af3c340e9763f5cecc5f0e601e1408352b0
kspace-trk <keigonakao.0825@gmail.com>
2025-02-10 19:58:01 +0900

    バリデーション前のコード作成

---
+++ app.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const getResult = await $fetch('/api', {
+  query: {
+    name: 'keigo',
+  },
+});
+
+const postResult = await $fetch('/api', {
+  method: 'POST',
+  body: {
+    name: 'keigo',
+  },
+});
+
+const routeParamResult = await $fetch('/api/keigo');
+
+</script>
+
+<template>
+  <div>
+    <div>
+      <p>GET結果: {{ getResult.message }}</p>
+    </div>
+    <div>
+      <p>POST結果: {{ postResult.message }}</p>
+    </div>
+    <div>
+      <p>ルートパラメータGET結果: {{ routeParamResult.message }}</p>
+    </div>
+  </div>
+</template>
---
+++ server/api/[name].get.ts
@@ -0,0 +1,6 @@
+export default defineEventHandler(async (event) => {
+  const name = getRouterParam(event, 'name');
+  return {
+    message: `Hello ${name} !`,
+  };
+});
